fa6c1f0661e6cf68ae8529d30d7b5e9d7f6e8e54
DanielSilveira0106 <103533078+DanielSilveira0106@users.noreply.github.com>
2022-04-12 16:54:19 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+var caixa;
+function setup() {
+  createCanvas(400,400);
+caixa=createSprite(200,200,20,20)
+}
+
+function draw() 
+{
+  background(200);
+if (keyIsDown(LEFT_ARROW)) {
+ caixa.position.x = caixa.position.x-2  
+}
+if (keyIsDown(RIGHT_ARROW)) {
+ caixa.position.x = caixa.position.x +2 
+}
+if (keyIsDown(UP_ARROW)){
+caixa.position.y = caixa.position.y -2  
+}
+ if (keyIsDown(DOWN_ARROW)) {
+  caixa.position.y = caixa.position.y +2 
+ }
+drawSprites()
+}
+
+
+
+
